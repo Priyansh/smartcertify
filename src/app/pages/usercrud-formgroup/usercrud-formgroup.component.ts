580e69bddce1d404447fab80d7601fcdb68d27f9
Priyansh <priyanshshah.bece@hotmail.com>
2025-09-04 00:25:25 -0400

    Update and Delete user and refactor upsert logic

--- src/app/pages/usercrud-formgroup/usercrud-formgroup.component.ts
+++ src/app/pages/usercrud-formgroup/usercrud-formgroup.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,10 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './usercrud-formgroup.component.html',
   styleUrl: './usercrud-formgroup.component.css'
 })
+// REFERENCE : https://www.youtube.com/watch?v=dlNY-eQOZ5s
+//RESOURCES : https://www.youtube.com/watch?v=PjMqb6tonqk&list=PL7JmcZV0UQtV2QuJXflBHWsS1pgYDyuC0&index=16
+// Testing API: https://freeprojectapi.com/ has all CRUD API call (i.e https://api.freeprojectapi.com/api/GoalTracker/getAllUsers)
+// Fake API : https://jsonplaceholder.typicode.com/users
 export class UsercrudFormgroupComponent implements OnInit {
   protected http = inject(HttpClient);
   protected userForm: FormGroup = new FormGroup({
@@ -19,6 +23,8 @@ export class UsercrudFormgroupComponent implements OnInit {
 
   });
   protected userAll: any[] = [];
+  protected hasEdit = signal(false);
+  protected userId = signal(0);
   protected base_URL = "https://jsonplaceholder.typicode.com";
 
   async ngOnInit() {
@@ -35,32 +41,82 @@ export class UsercrudFormgroupComponent implements OnInit {
     });
   }
 
-  onEditUser(id: number) {
-    this.http.get(this.base_URL + '/users/' + id).subscribe((user: any) => {
+  onEditUser(user: any) {
+    /* this.http.get(this.base_URL + '/users/' + id).subscribe((user: any) => {
       this.userForm = new FormGroup({
         id: new FormControl(user.id),
         name: new FormControl(user.name),
         username: new FormControl(user.username),
         email: new FormControl(user.email),
       });
-    })
-  }
-  onSubmitUsers() {
-    var userData = this.userForm.value;
-    this.http.post(this.base_URL + '/users', userData).subscribe({
-      next: (data) => {
-        alert('User created successfully!');
-        this.userForm.reset();
-      },
-      error: (error) => {
-        console.error('Error creating user:', error);
-        alert('Error creating user. Please try again.');
-      }
+      this.userId.set(id);
+      this.hasEdit.set(true);
+    }) */
+    this.userForm = new FormGroup({
+      id: new FormControl(user.id),
+      name: new FormControl(user.name),
+      username: new FormControl(user.username),
+      email: new FormControl(user.email),
     });
-    this.onGetUserAll();
+    this.userId.set(user.id);
+    this.hasEdit.set(true);
   }
 
-  onClear(){
+  onUpsertUsers() {
+    var userData = this.userForm.value;
+    if (!this.hasEdit()) {
+      this.http.post(this.base_URL + '/users', userData).subscribe({
+        next: (data) => {
+          alert('User created successfully!');
+          this.userForm.reset();
+        },
+        error: (error) => {
+          console.error('Error creating user:', error.error);
+          alert('Error creating user:' + error.error);
+        }
+      });
+    }
+    else {
+      // Call update form
+      console.log(`In Update and see updated User Id : ${this.userId()}`);
+      this.http.put(this.base_URL + '/users/' + this.userId(), userData).subscribe({
+        next: (data) => {
+          alert('User updated successfully!');
+          this.userForm.reset();
+        },
+        error: (error) => {
+          console.error('Error updating user:', error.error);
+          alert('Error updating user: ' + error.error);
+        }
+      });
+
+    }
+
+    //after submit ,let's repopulate table by calling GetUserAll and Reset form(or clean up)
+    this.onGetUserAll();
+    this.onClear();
+
+  }
+
+  onDeleteUser(id: number) {
+    var hasDeleteRequested: Boolean = confirm("Are you sure you want to Delete ?");
+    if (hasDeleteRequested) {
+      this.http.delete(this.base_URL + '/users/' + id).subscribe({
+        next: (data) => {
+          alert('User deleted successfully!');
+          this.onGetUserAll();
+        },
+        error: (error) => {
+          alert('Error deleting user: ' + error.error);
+        }
+      });
+    }
+
+  }
+
+  onClear() {
+    this.hasEdit.set(false);
+    this.userId.set(0);
     this.userForm.reset();
   }
 }
